75994ebf1c0f2f693010ab22c78a632ad17f881a
Franco72 <francomoya1022@gmail.com>
2021-08-11 23:48:59 -0300

    third commit

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
+const finnhub = require("finnhub");
+const api_key = finnhub.ApiClient.instance.authentications["api_key"];
+api_key.apiKey = "c49eptiad3ieskgqte60";
 (function main() {
+  //Inicializating Finnhub API Client
+  const finnhubClient = new finnhub.DefaultApi();
+
+  //   finnhubClient.quote("AAPL", (error, data, response) => {
+  //     console.log(data);
+  //   });
+  finnhubClient.symbolSearch("apple", (error, data, response) => {
+    console.log(data);
+  });
 })();
